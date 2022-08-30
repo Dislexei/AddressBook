7bd14821eb904abc801e4977748dc2f2819ac3f8
Oleksii <alexeistrapchev@gmail.com>
2022-08-31 00:34:42 +0200

    Changed some routing, adding salutation enum to people class

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,7 @@
 class Person < ApplicationRecord
-	enum salutation: {
-		Mr:  0,
-		Mrs: 1,
-		Ms:  2
-	}, _prefix: true
-	has_many :emailaddress
-	has_many :phone_numbers
-	has_many :addresses
-	has_one :salutation
+	enum salutation: {Mr:0, Mrs:1, Ms:2, Dr:3}
+	# has_many :emailaddress
+	# has_many :phone_numbers
+	# has_many :addresses
+	
 end
